f7b2db926b670f4a6f61e0a50f8d0d12581f6825
siun <siuncyclone@gmail.com>
2014-07-03 23:05:45 +0900

    Add 097.rb

---
+++ 097.rb
@@ -0,0 +1,15 @@
+#2012/5/26
+#20min
+
+def exponent_under10(n, m)
+  return n if m==1
+  (2..m).each{|i|
+    n *= 2
+    n = n.to_s[-10,10].to_i if n.to_s.split(//).size > 10
+  }
+  return n
+end
+
+start = Time.now
+p (28433*exponent_under10(2,7830457)+1).to_s[-10,10].to_i
+p Time.now-start
