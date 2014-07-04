d2da092a4b84c2bd4c421baa6c99635d3a613766
siun <siuncyclone@gmail.com>
2014-07-04 17:40:14 +0900

    Update 017.rb

--- 017.rb
+++ 017.rb
@@ -1,27 +1,14 @@
-str = [3,3,5,4,4,3,5,5,4,3,6,6,8,8,7,7,9,8,8] 
-num = [6,6,5,5,5,7,6,6]
-ary = num.map do |x| 
-  (0..9).map do |i|
-    if i != 0
-      str[i-1] + x
-    else
-      x
-    end
-  end
-end
-ary.flatten!
-ary.each{|x| str << x }
+list = [3,3,5,4,4,3,5,5,4,3,6,6,8,8,7,7,9,8,8] 
 
-re = []
-9.times do |i| 
-  (0..str.size).each do |x|
-    if x==0
-      re << (7 + str[i])
-    else
-      re << (10 + str.slice(0..8)[i] + str[x-1])
-    end
+[6,6,5,5,5,7,6,6].each do |x| 
+  (0..9).each do |i|
+    list << (i != 0 ? list[i-1] + x : x)
   end
 end
-re.each{|x| str << x }
-str << 11
-p str.inject{|sum, x| sum + x }
+
+9.times do |i| 
+  (0..99).each do |x|
+    list << (x == 0 ? 7 + list[i] : (10 + list.slice(0..8)[i] + list[x-1]))
+  end
+end
+p (list << "one thousand".split.join.length).inject{|sum, x| sum + x }
