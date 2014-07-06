9d872980ed8f5f24d78e3f1ddee1569b9437208f
siun <siuncyclone@gmail.com>
2014-07-06 20:38:49 +0900

    Update 067.rb

--- 067.rb
+++ 067.rb
@@ -1,29 +1,9 @@
-require './tool'
+str = open("./files/067/triangle.txt"){|f| f.read.scan(/[0-9][^\n\r]+/).map{|x| x.scan(/\w+/).map(&:to_i) } }.reverse
 
-str = nil
-open("./files/067/triangle.txt") do |f|
-  str = f.read
+def trimax(seed, child)
+  seed.map.with_index{|x, i| x + [child[i], child[i+1]].max }
 end
-line_num = []
-str.scan(/([^\n]+)/).each do |line|
-  line.each do |x|
-    line_num << x.scan(/[0-9]+/)
-  end
-end
-line_num.reverse!
-ary,stk = [],[]
 
-0.upto(line_num.size-2) do
-  0.upto(line_num[0].size-1) do |i|
-    ary << ((line_num[0][i].to_i > line_num[0][i+1].to_i) ? line_num[0][i] : line_num[0][i+1])
-  end
-  line_num.shift
-  line_num[0].each.with_index do |x,i|
-    stk << x.to_i+ary[i].to_i.deep_clone
-  end
-  line_num.shift
-  line_num.unshift(stk.deep_clone)
-  ary.clear
-  stk.clear
-end
-puts line_num
+list = str.first
+p (1...str.length).map{|i| list = trimax(str[i], list) }.flatten.last
+
